3bb774e5a4bcb81160967dac5280b6d60b305b60
Ranis Zyalalov <Jabelya@yandex.ru>
2025-04-26 07:44:42 +0300

    Update

--- main-service/src/main/java/ewm/request/ParticipationRequestRepository.java
+++ main-service/src/main/java/ewm/request/ParticipationRequestRepository.java
@@ -3,7 +3,6 @@ package ewm.request;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface ParticipationRequestRepository extends JpaRepository<ParticipationRequest, Long> {
     Collection<ParticipationRequest> findByRequesterId(Long requesterId);
